312ffe5d5954332b7191bc28a4d832bbb0c14a6a
Abdullah Saeed <bnbshrbconect@gmail.com>
2026-05-07 17:09:40 +0300

    Create Instagram webhooks

--- backend/main.py
+++ backend/main.py
@@ -7,33 +7,33 @@ from arq import create_pool
 from arq.connections import RedisSettings
 from contextlib import asynccontextmanager
 
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
-stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
+# stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    pool = await create_pool(RedisSettings())
-    app.state.arq_pool = pool
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     pool = await create_pool(RedisSettings())
+#     app.state.arq_pool = pool
 
-    try:
-        await pool.ping()
-        redis_ok = True
-    except Exception:
-        redis_ok = False
+#     try:
+#         await pool.ping()
+#         redis_ok = True
+#     except Exception:
+#         redis_ok = False
 
-    if not redis_ok:
-        raise RuntimeError("Redis is not working")
+#     if not redis_ok:
+#         raise RuntimeError("Redis is not working")
 
-    worker_keys = await pool.keys("arq:queue:*")
-    if not worker_keys:
-        raise RuntimeError("No ARQ workers detected")
+#     worker_keys = await pool.keys("arq:queue:*")
+#     if not worker_keys:
+#         raise RuntimeError("No ARQ workers detected")
     
-    yield
-    await pool.close()
+#     yield
+#     await pool.close()
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI() #(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -46,8 +46,8 @@ app.add_middleware(
 
 from routers import booking, webhook, test
 
-app.include_router(test.router, prefix="/test", tags=["test"])
-app.include_router(booking.router, prefix="/booking", tags=["booking"])
+# app.include_router(test.router, prefix="/test", tags=["test"])
+# app.include_router(booking.router, prefix="/booking", tags=["booking"])
 app.include_router(webhook.router, prefix="/webhook", tags=["webhook"])
 
 if __name__ == "__main__":
