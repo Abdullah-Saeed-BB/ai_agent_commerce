110da2aeb085561c46f136917c5908dbd020fedc
Abdullah Saeed <bnbshrbconect@gmail.com>
2026-05-10 00:31:02 +0300

    Apply all database operations of booking in booking_service.py

---
+++ backend/ai_agent.py
@@ -0,0 +1,303 @@
+from langgraph.graph.state import CompiledStateGraph
+from langchain_core.runnables import Runnable
+from langchain_core.messages import BaseMessage, ToolMessage, SystemMessage, HumanMessage, AIMessage
+from langchain_core.tools import tool
+from langchain.chat_models import init_chat_model
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from langgraph.graph import StateGraph, END
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode
+from typing import Annotated, Sequence, TypedDict, List, Dict, Optional, Literal, Union
+
+from sqlalchemy import text, select, and_
+from pathlib import Path
+import datetime
+import pandas as pd
+import chromadb
+import os
+
+
+from db.session import AsyncSessionLocal
+from db.models import Booking, Barber
+from services.booking_service import get_availability, create_booking
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+BASE_DIR = Path(__file__).parent
+
+PERSIST_DIRECTORY = BASE_DIR / ".data" / "silver_blade_vdb"
+MODEL_PATH = BASE_DIR / "models" / "embedding_function"
+
+VDB_HOST = os.getenv("VDB_HOST")
+VDB_PORT = os.getenv("VDB_PORT")
+
+client = chromadb.HttpClient(host=VDB_HOST, port=VDB_PORT)
+
+print("Loading embedding function locally ...")
+embedding_function = HuggingFaceEmbeddings(model=str(MODEL_PATH))
+
+print("Creating Vector Store ...")
+vector_store = Chroma(
+    client=client,
+    persist_directory=PERSIST_DIRECTORY,
+    collection_name="silver-blade-vdb",
+    embedding_function=embedding_function
+)
+
+@tool
+def retriever_tool(
+    filter: Union[Literal["barber", "service", "business_info"], None], query: str) -> str:
+    """
+    To searches and returns the information from the Silver Blade Business Knowledge base document.
+    Arqs:
+        query: To search for specific information, do not make the query vague, add details if user did not provided
+        filter: To search only for specific data, `barber` To search in barbers data, `service` To search in services data, and `business_info` to search in business knowledge base,
+                use `None` if user wanna know about different thing and need to search in different data. 
+    """
+
+    if filter:
+        retriever = vector_store.as_retriever(
+            search_kwargs={
+                "k": 2 if filter == "business_info" else 5,
+                "filter": {"type": filter}
+            }
+        )
+    else:
+        retriever = vector_store.as_retriever(
+            search_kwargs={"k": 2}
+        )
+
+    docs = retriever.invoke(query)
+
+    if not docs:
+        return "I found no relevant information in the Stock Market Performance 2024 document."
+
+    results = []
+    for i, doc in enumerate(docs):
+        doc_type = doc.metadata["type"]
+        title = doc_type.replace("_", " ").capitalize()
+        results.append(f"{title} {i+1}:\n{doc.page_content}")
+    return "\n\n".join(results)
+
+@tool
+async def find_free_times_tool(
+    start: str,
+    end: str,
+    limit: Optional[int] = None
+) -> str:
+    """
+    Find free booking time slots between start and end dates by checking the database.
+    Args:
+        start: Start date in YYYY-MM-DD format
+        end: End date in YYYY-MM-DD format
+        limit: Optional max number of slots to return
+    """
+
+    # 1. Parse dates
+    start_date = datetime.datetime.strptime(start, "%Y-%m-%d").date()
+    end_date = datetime.datetime.strptime(end, "%Y-%m-%d").date()
+
+    async with AsyncSessionLocal() as db: 
+        availability_data = await get_availability(db, start_date, end_date)
+
+    if not availability_data:
+        return "No barbers are available for the selected dates."
+
+    # Format Output
+    title = f"Here is {'the first ' + str(limit) if limit else ''} free times {'at *' + start + '*' if start == end else 'from *' + start + '* to *' + end + '*'}:"
+    final_output = [title]
+    
+    count = 0
+    for day_data in availability_data:
+        date_str = day_data["date"]
+        day_header = f"*Free times {date_str}:*"
+        time_strings = []
+        for slot in day_data["available_slots"]:
+            names_fmt = ", ".join([f"'{b['name']}'" for b in slot['available_barbers']])
+            
+            # Format time to 12h
+            t_obj = datetime.datetime.strptime(slot['time'], "%H:%M").time()
+            t_str = t_obj.strftime("%I:%M %p").lstrip("0") if t_obj.strftime("%I")[0] == '0' else t_obj.strftime("%I:%M %p")
+            
+            time_strings.append(f"{t_str} ({names_fmt})")
+            
+            count += 1
+            if limit and count >= limit:
+                break
+        
+        final_output.append(f"{day_header} {', '.join(time_strings)}")
+        if limit and count >= limit:
+            break
+
+    return "\n".join(final_output)
+
+
+def validate_booking_time(dt_str: str) -> tuple[bool, datetime.datetime | str]:
+    try:
+        # 1. Parse the ISO string
+        clean_dt_str = dt_str.replace('Z', '+00:00')
+        dt = datetime.datetime.fromisoformat(clean_dt_str)
+        
+        # 2. Business Logic Checks
+        if not (9 <= dt.hour < 18):
+            return False, "Time must be between 9:00 AM and 6:00 PM."
+        
+        if dt.minute not in [0, 30]:
+            return False, "Appointments must be on the hour or half-hour."
+
+        # 3. FIX: Strip the timezone to make it "offset-naive"
+        # This resolves the (can't subtract offset-naive and offset-aware datetimes) error
+        dt_naive = dt.replace(tzinfo=None)
+            
+        return True, dt_naive
+    except ValueError:
+        return False, "Invalid format. Use YYYY-MM-DDThh:mm:ssZ."
+
+@tool
+async def booking_tool(
+    name: str | None = None,
+    booking_datetime: str | None = None,
+    service: str | None = None,
+    barber: str | None = None,
+):
+    """
+    To book an appointment to the barbershop.
+    Args:
+        name: The name of the user if mentioned in the conversation.
+        booking_datetime: The booking_datetime as "YYYY-MM-DDThh:mm:ssZ" if mentioned in the conversation.
+        service: The service name or title (e.g., 'Haircut', 'Beard Trim') if mentioned in the conversation.
+        barber: The barber name if mentioned in the conversation.
+    """
+    
+    missing_info = []
+    validated_data = {
+        "name": name,
+        "booking_datetime": None,
+        "service_id": None,
+        "barber_id": None
+    }
+
+    # 1. Validate Name
+    if not name:
+        missing_info.append("your name")
+
+    # 2. Validate Datetime
+    if booking_datetime:
+        is_valid, dt_result = validate_booking_time(booking_datetime)
+        if is_valid:
+            validated_data["booking_datetime"] = dt_result
+        else:
+            missing_info.append(f"a valid date & time")
+    else:
+        missing_info.append("date & time")
+
+    # 3. Validate Service (Vector Search for ID)
+    if service:
+        try:
+            retriever = vector_store.as_retriever(
+                search_kwargs={'k': 1, 'filter': {'type': 'service'}}
+            )
+            documents = retriever.invoke(service)
+            if documents:
+                service_doc = documents[0]
+                validated_data["service_id"] = service_doc.metadata["id"]
+            else:
+                missing_info.append("a valid service from our menu")
+        except Exception as e:
+            print("Error while parsing service data:", e)
+            missing_info.append("service identification")
+    else:
+        missing_info.append("service title")
+
+    # 4. Validate Barber (Vector Search for ID)
+    if barber:
+        try:
+            retriever = vector_store.as_retriever(
+                search_kwargs={'k': 1, 'filter': {'type': 'barber'}}
+            )
+            documents = retriever.invoke(barber)
+            if documents:
+                validated_data["barber_id"] = documents[0].metadata["id"]
+            else:
+                missing_info.append("an available barber name")
+        except Exception:
+            missing_info.append("barber identification")
+    else:
+        missing_info.append("barber name")
+
+    try:
+        async with AsyncSessionLocal() as db:
+            new_booking = await create_booking(
+                db=db,
+                customer_name=validated_data["name"],
+                booking_datetime=validated_data["booking_datetime"],
+                service_id=validated_data["service_id"],
+                barber_id=validated_data["barber_id"],
+                payment_id=None
+            )
+    except Exception as e:
+        print(e)
+        return "Error occurs while creating the booking to the database, try again later"
+
+    payment_url = f"{os.getenv("FRONTEND_URL")}/payment?id={new_booking.id}"
+    if not missing_info:
+        return f"*Creating booking successed*, just need to confirm the pay.\n\n[Payment page]({payment_url})"
+    else:
+        readable_missing = \
+            f"{", ".join(missing_info[:-1])}, and {missing_info[-1]}" 
+        return f"*Creating booking successed*, you have to enter {readable_missing} in payment page before confirming the pay.\n\n[Payment Page]({payment_url})"
+
+
+# Initilization the AI Agent
+llm = init_chat_model(
+    model="openai/gpt-oss-120b",
+    model_provider="groq",
+    temperature=0
+)
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+
+tools = [find_free_times_tool, retriever_tool, booking_tool]
+
+model = llm.bind_tools(tools)
+
+def call_llm(state: AgentState) -> AgentState:
+    """Function to call the LLM with the current state."""
+    messages = list(state['messages'])
+    message = model.invoke(messages)
+    return {'messages': [message]}
+
+def should_call_tool(state: AgentState):
+    last_message = state["messages"][-1]
+
+    return "tools" if last_message.tool_calls else END
+
+def should_continue(state: AgentState):
+    last_message = state["messages"][-1]
+
+    if last_message.name in ("find_free_times_tool", "booking_tool"):
+        return END
+    return "agent"
+
+graph = StateGraph(AgentState)
+graph.add_node("agent", call_llm)
+graph.add_node("tools", ToolNode(tools))
+
+graph.set_entry_point("agent")
+graph.add_conditional_edges(
+    "agent",
+    should_call_tool,
+    {"tools": "tools", END: END}
+)
+
+graph.add_conditional_edges(
+    "tools",
+    should_continue,
+    {"agent": "agent", END: END}
+)
+
+app: CompiledStateGraph = graph.compile()
---
+++ backend/services/booking_service.py
@@ -0,0 +1,119 @@
+import uuid
+from typing import Optional, List, Dict, Any
+from datetime import date, datetime, timedelta
+from collections import defaultdict
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, and_, func
+
+from db.models import Booking, Barber, PaymentStatus
+from services.get_slots import generate_slots
+
+async def get_availability(
+    db: AsyncSession,
+    start_date: date,
+    end_date: date,
+    barber_id: Optional[uuid.UUID] = None
+) -> List[Dict[str, Any]]:
+    """
+    Return available time slots between start_date and end_date.
+    If barber_id is provided, only return slots where that barber is available.
+    Otherwise, return slots where at least one barber is available.
+    """
+    all_slots = generate_slots()
+
+    # 1. Fetch active barbers
+    barber_stmt = select(Barber)
+    if barber_id:
+        barber_stmt = barber_stmt.where(Barber.id == barber_id)
+    
+    barbers_result = await db.execute(barber_stmt)
+    active_barbers = barbers_result.scalars().all()
+    
+    if not active_barbers:
+        return []
+    
+    barber_lookup = {b.id: b.name for b in active_barbers}
+
+    # 2. Fetch existing bookings
+    stmt = (
+        select(Booking)
+        .where(
+            and_(
+                func.date(Booking.booking_datetime) >= start_date,
+                func.date(Booking.booking_datetime) <= end_date,
+                Booking.payment_status.in_([PaymentStatus.PENDING, PaymentStatus.SUCCESSFUL])
+            )
+        )
+    )
+    result = await db.execute(stmt)
+    bookings = result.scalars().all()
+
+    # 3. Create map of {date: {time: set(booked_barber_ids)}}
+    booked_map = defaultdict(lambda: defaultdict(set))
+    for b in bookings:
+        d_key = b.booking_datetime.date()
+        t_key = b.booking_datetime.time()
+        booked_map[d_key][t_key].add(b.barber_id)
+
+    # 4. Build availability structure
+    available_data = []
+    
+    current_date = start_date
+    while current_date <= end_date:
+        day_availability = []
+        for slot_time in all_slots:
+            free_barbers = [
+                {"id": b_id, "name": b_name}
+                for b_id, b_name in barber_lookup.items()
+                if b_id not in booked_map[current_date][slot_time]
+            ]
+            
+            if free_barbers:
+                day_availability.append({
+                    "time": slot_time.strftime("%H:%M"),
+                    "available_barbers": free_barbers
+                })
+                
+        if day_availability:
+            available_data.append({
+                "date": current_date.isoformat(),
+                "available_slots": day_availability
+            })
+            
+        current_date += timedelta(days=1)
+
+    return available_data
+
+
+async def create_booking(
+    db: AsyncSession,
+    customer_name: Optional[str] = None,
+    booking_datetime: Optional[datetime] = None,
+    service_id: Optional[uuid.UUID] = None,
+    barber_id: Optional[uuid.UUID] = None,
+    payment_id: Optional[str] = None,
+    arq_pool=None
+) -> Booking:
+    """
+    Create a new booking and optionally schedule an auto-cancel task.
+    """
+    new_booking = Booking(
+        payment_id=payment_id,
+        service_id=service_id,
+        barber_id=barber_id,
+        booking_datetime=booking_datetime,
+        customer_name=customer_name,
+    )
+    db.add(new_booking)
+    await db.commit()
+    await db.refresh(new_booking)
+
+    if arq_pool:
+        await arq_pool.enqueue_job(
+            "cancel_booking_task",
+            booking_id=str(new_booking.id),
+            _defer_by=30 * 60,
+        )
+
+    return new_booking
